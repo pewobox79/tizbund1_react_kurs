90ebe31588ca58392b11723ea18862380d0c758f
aviareps_pwolf <pwolf@aviareps.com>
2026-06-12 12:10:03 +0200

    authupdated

--- react-ts/src/auth/auth.ts
+++ react-ts/src/auth/auth.ts
@@ -1,5 +1,6 @@
 export function isAuthenticated(){
-
-    //=> infos aus localStorage
-    return false
+    // an dieser stelle kann KEIN CUSTOM HOOK verwendet werden. 
+    const user = localStorage.getItem("itzb_user")
+    const userData = user ? JSON.parse(user) : {loggedIn: false}
+    return userData?.loggedIn
 }
--- react-ts/src/layouts/ProtectedLayout.tsx
+++ react-ts/src/layouts/ProtectedLayout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router"
+import { useLocalStorage } from "../hooks/useLocalStorage"
 
 const ProtectedLayout =()=>{
 
+    const {value} = useLocalStorage("itzb_user")
+
     return <div style={{border: "1px solid red"}}>
-        <h1>protected layout</h1>
+        <h1>protected layout {value.username}</h1>
         <Outlet/>
     </div>
 }
